bc7d0d4a49b9de1105035847ae7ed87eed4d5c6f
Greg Perry <gsperry2011@gmail.com>
2017-06-15 12:48:02 -0400

    edited init.el to add chrome default browser, added comments.
    also edited .Xresources to up font size

--- testvm/init.el
+++ testvm/init.el
@@ -1,5 +1,14 @@
+
+; default turn on line numbers
 (global-linum-mode t)
+
+; adding space between line numbers and lines
 (setq linum-format "%d ")
+
+;disable these bars of junk
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+
+; set default browser for org mode links to "chrome" if chrome is the default xdg browser
+(setq browse-url-browser-function 'browse-url-xdg-open)
