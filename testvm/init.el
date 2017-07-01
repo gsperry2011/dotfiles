69002c78b54a935d9749302013e58c47c299bdcb
Greg Perry <gsperry2011@gmail.com>
2017-06-30 23:07:48 -0400

    added melpa to init.el

--- testvm/init.el
+++ testvm/init.el
@@ -1,3 +1,14 @@
+; enable melpa (with stable archive)
+
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
 
 ; default turn on line numbers
 (global-linum-mode t)
@@ -12,3 +23,6 @@
 
 ; set default browser for org mode links to "chrome" if chrome is the default xdg browser
 (setq browse-url-browser-function 'browse-url-xdg-open)
+
+; default company-mode in all buffers
+(add-hook 'after-init-hook 'global-company-mode)
