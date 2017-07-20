ff92c4f8a6ca5c7888b5e836faeba387f69b1587
Greg Perry <greg.perry@encore.tech>
2017-07-20 15:36:10 -0400

    increased font size from 12 to 15 in .Xresources. added personal i3 settings into config for floating windows, keybinds, auto exec on login.

--- testvm/init.el
+++ testvm/init.el
@@ -73,7 +73,6 @@
 ; set default browser for org mode links to "chrome" if chrome is the default xdg browser
 (setq browse-url-browser-function 'browse-url-xdg-open)
 
-<<<<<<< HEAD
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -86,7 +85,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-=======
+
 ;;;;;;;;;;;;;;;;;;
 ;; transparency ;;
 ;;;;;;;;;;;;;;;;;;
@@ -96,5 +95,3 @@
  (set-frame-parameter (selected-frame) 'alpha '(85 . 50))
  (add-to-list 'default-frame-alist '(alpha . (85 . 50)))
 
-
->>>>>>> 5c02302bcff5b0efa60284f09f66aac906cd7897
