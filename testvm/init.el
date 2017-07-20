0eec5450f72c348440805bea82b88efffe219ad8
Greg Perry <greg.perry@encore.tech>
2017-07-20 15:11:32 -0400

    added emacs functionality for helm (requires emacs 24.4+)

--- testvm/init.el
+++ testvm/init.el
@@ -70,3 +70,15 @@
 ; set default browser for org mode links to "chrome" if chrome is the default xdg browser
 (setq browse-url-browser-function 'browse-url-xdg-open)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (w3 helm use-package company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
