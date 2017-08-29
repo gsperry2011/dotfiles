5160fe8891410a468b1cd1044a87d296405a8400
Greg Perry <gsperry2011@gmail.com>
2017-08-29 18:37:52 -0400

    added ox-confluence

--- testvm/init.el
+++ testvm/init.el
@@ -81,6 +81,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-export-backends (quote (ascii beamer html icalendar latex odt confluence)))
  '(package-selected-packages (quote (w3 helm use-package company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -98,3 +99,8 @@
  (set-frame-parameter (selected-frame) 'alpha '(85 . 50))
  (add-to-list 'default-frame-alist '(alpha . (85 . 50)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  org-export         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'ox-confluence)
