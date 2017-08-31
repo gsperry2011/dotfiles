a9e9fa23dcf30ce10e55b87281fe080371e9b606
Greg Perry <gsperry2011@gmail.com>
2017-08-30 20:18:23 -0400

    removed transparency as it conflicts with compton

--- testvm/init.el
+++ testvm/init.el
@@ -107,8 +107,8 @@
 
  ;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
  ;;(set-frame-parameter (selected-frame) 'alpha <both>)
- (set-frame-parameter (selected-frame) 'alpha '(85 . 50))
- (add-to-list 'default-frame-alist '(alpha . (85 . 50)))
+;; (set-frame-parameter (selected-frame) 'alpha '(85 . 50))
+;; (add-to-list 'default-frame-alist '(alpha . (85 . 50)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;   org-export        ;;
