c238b3ecde8e65b4442796f03d91a8aa64dd7295
Greg Perry <gsperry2011@gmail.com>
2017-09-23 01:27:39 -0400

    added org-bullets, helm-mode

--- laptop/init.el
+++ laptop/init.el
@@ -93,7 +93,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org python w3 helm use-package company))))
+ '(package-selected-packages
+   (quote
+    (org-bullets ledger-mode org python w3 helm use-package company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -107,11 +109,28 @@
 
  ;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
  ;;(set-frame-parameter (selected-frame) 'alpha <both>)
- (set-frame-parameter (selected-frame) 'alpha '(85 . 50))
- (add-to-list 'default-frame-alist '(alpha . (85 . 50)))
+;; (set-frame-parameter (selected-frame) 'alpha '(85 . 50))
+;; (add-to-list 'default-frame-alist '(alpha . (85 . 50)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;   org-export        ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'ox-confluence)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;    org-bullets      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;     helm-mode       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'helm-config)
+(helm-mode 1)
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
