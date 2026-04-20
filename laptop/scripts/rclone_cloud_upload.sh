ae41e9bb3d900f15a0a9b35ae8803aaee0c4c389
Greg Perry <greg.perry@encore.tech>
2026-04-20 12:27:11 -0400

    use new rclone bisync

--- laptop/scripts/rclone_cloud_upload.sh
+++ laptop/scripts/rclone_cloud_upload.sh
@@ -2,13 +2,17 @@
 
 RCLONE_CONFIG=/home/greg/.config/rclone/rclone.conf
 export RCLONE_CONFIG
+LOCAL_DIR="/home/greg/googledrive/"
+REMOTE_NAME="google-drive:"
 
 #exit if running
 if [[ "`pidof -x $(basename $0) -o %PPID`" ]]; then exit; fi
 
-# Push local files to google drive
-/usr/bin/rclone copy -u /home/greg/googledrive/ google-drive:
-
-# Pull files from google drive to local
-/usr/bin/rclone copy -u google-drive: /home/greg/googledrive/
+# Bidirectional sync
+# --resilient: allows it to continue on minor errors
+# --drive-skip-gdocs: avoids errors with native Google docs
+/usr/bin/rclone bisync "$LOCAL_DIR" "$REMOTE_NAME" \
+    --resilient \
+    --drive-skip-gdocs \
+    --verbose
 
